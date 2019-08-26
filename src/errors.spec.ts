0994750b583d69d710ec2b22368b52d48a43df9f
Jonas Lalin <1ac6df24@gmail.com>
2019-08-26 09:41:16 +0200

    add tests for errors

---
+++ src/errors.spec.ts
@@ -0,0 +1,76 @@
+import { defer, of, throwError } from 'rxjs';
+
+describe('errors', () => {
+  let nextSpy;
+  let errorSpy;
+  let completeSpy;
+
+  beforeEach(() => {
+    nextSpy = jest.fn();
+    errorSpy = jest.fn();
+    completeSpy = jest.fn();
+  });
+
+  describe('should invoke next and complete callbacks', () => {
+    test('of(new Error())', () => {
+      const source = of(new Error());
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).toHaveBeenCalled();
+      expect(errorSpy).not.toHaveBeenCalled();
+      expect(completeSpy).toHaveBeenCalled();
+    });
+
+    test(`of('it borked')`, () => {
+      const source = of('it borked');
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).toHaveBeenCalled();
+      expect(errorSpy).not.toHaveBeenCalled();
+      expect(completeSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('should only invoke the error callback', () => {
+    test('throwError(new Error())', () => {
+      const source = throwError(new Error());
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).not.toHaveBeenCalled();
+      expect(errorSpy).toHaveBeenCalled();
+      expect(completeSpy).not.toHaveBeenCalled();
+    });
+
+    test(`throwError('it borked')`, () => {
+      const source = throwError('it borked'); // lacks stack trace information
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).not.toHaveBeenCalled();
+      expect(errorSpy).toHaveBeenCalled();
+      expect(completeSpy).not.toHaveBeenCalled();
+    });
+
+    test('throw new Error()', () => {
+      const source = defer(() => {
+        throw new Error();
+      });
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).not.toHaveBeenCalled();
+      expect(errorSpy).toHaveBeenCalled();
+      expect(completeSpy).not.toHaveBeenCalled();
+    });
+
+    test(`throw 'it borked'`, () => {
+      const source = defer(() => {
+        // tslint:disable-next-line:no-string-throw
+        throw 'it borked'; // lacks stack trace information
+      });
+      source.subscribe(nextSpy, errorSpy, completeSpy);
+
+      expect(nextSpy).not.toHaveBeenCalled();
+      expect(errorSpy).toHaveBeenCalled();
+      expect(completeSpy).not.toHaveBeenCalled();
+    });
+  });
+});
